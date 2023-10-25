01357a280513137371743f2443f3be927fd82505
Han Kim <han.dhk.kim@gmail.com>
2023-10-24 19:12:49 -0700

    Completed Exercise 3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -35,6 +35,15 @@ app.get('/api/persons', (request, response) => {
   response.json(notes)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(n => n.id === id)
+  if(note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
